11e093f73d1722cf05faf8b6551d55033e6c0ff9
nicolelLaurian.anistoroaei@datasentics.com <nicolelLaurian.anistoroaei@datasentics.com>
2023-09-11 10:20:38 +0000

    refactored sociodemo notebook

--- features_temp/sociodemo/sociodemo.py
+++ features_temp/sociodemo/sociodemo.py
@@ -12,73 +12,43 @@
 # COMMAND ----------
 
 import mlflow
+import logging
 import pyspark.sql.functions as F
 from logging import Logger
 from pyspark.sql.dataframe import DataFrame
-from pyspark.sql import types as T
+# pylint: disable=W0614
+# pylint: disable=W0401
+from pyspark.sql.types import * 
 from pyspark.sql.window import Window
-from typing import List
 
-from src.utils.helper_functions_defined_by_user import URL_lists 
 from src.utils.helper_functions_defined_by_user._functions_ml import ith
 from src.utils.helper_functions_defined_by_user._functions_helper import replace_categorical_levels
 from src.utils.helper_functions_defined_by_user.yaml_functions import get_value_from_yaml
-from src.utils.helper_functions_defined_by_user.logger import instantiate_logger
+# pylint: disable=W0621
+# pylint: disable=W0123
+# pylint: disable=W1514
+# pylint: disable=W0614
 
 # COMMAND ----------
 
-# MAGIC %md 
-# MAGIC
-# MAGIC
-# MAGIC ## Config
-# MAGIC `ALL_URL_LISTS` is a collected list of all domain URLs of interest (for individual gender and age categories).
-# MAGIC
-# MAGIC `PREFIX_LIST` is a list of strings with URL feature prefixes for easier feature creation. 
-# MAGIC
-# MAGIC `ALL_URL_LISTS_CLEANED` and `ALL_URLS_CLEANED` are lists of cleaned URL domains (periods in URL replaced by underscores) for better column naming.
-# MAGIC
-# MAGIC The remaining lists are lists of rarely occuring levels of categorical features with low significance - they are replaced with the `None` category before fitting to reduce the number of (rather useless) coefficients.
+logger = logging.getLogger("py4j")
+logger.setLevel(logging.ERROR)
+
+logger = spark._jvm.org.apache.log4j
+logging.getLogger("py4j.java_gateway").setLevel(logging.ERROR)
 
 # COMMAND ----------
-
-def _clean_url_list(url_list):
-    return [domain.replace(".", "_") for domain in url_list]
-
-# COMMAND ----------
-
-root_logger = instantiate_logger()
-
-# COMMAND ----------
-
-ALL_URL_LISTS = [
-    URL_lists.men_primary_url,
-    URL_lists.men_secondary_url,
-    URL_lists.women_primary_url,
-    URL_lists.women_secondary_url,
-    URL_lists.cat0_primary_url,
-    URL_lists.cat0_secondary_url,
-    URL_lists.cat1_primary_url,
-    URL_lists.cat1_secondary_url,
-    URL_lists.cat2_primary_url,
-    URL_lists.cat2_secondary_url,
-    URL_lists.cat3_primary_url,
-    URL_lists.cat3_secondary_url,
-    URL_lists.cat4_primary_url,
-    URL_lists.cat4_secondary_url,
-    URL_lists.cat5_primary_url,
-    URL_lists.cat5_secondary_url,
-    URL_lists.cat6_primary_url,
-    URL_lists.cat6_secondary_url,
-]
 
 PREFIX_LIST = ["men", "women", *[f"cat{_index}" for _index in range(7)]]
-
-ALL_URL_LISTS_CLEANED = [_clean_url_list(lst) for lst in ALL_URL_LISTS]
-ALL_URLS_CLEANED = set([domain for lst in ALL_URL_LISTS_CLEANED for domain in lst])
 
 SEARCH_ENGINE_VALUES_TO_REPLACE = ["ask", "maxask", "volny", "poprask"]
 DEVICE_OS_VALUES_TO_REPLACE = ["ipados", "ios"]
 DEVICE_TYPE_VALUES_TO_REPLACE = ["TV"]
+
+ALLOWED_VALUES = {"web_analytics_device_type_most_common_7d": ["mobile", "desktop"],
+"web_analytics_device_os_most_common_7d": ["android", "windows", "ios", "macos", "linux"],
+"web_analytics_device_browser_most_common_7d": ["chrome", "safari", "edge", "mozilla"],
+"web_analytics_page_search_engine_most_common_7d": ["google", "seznam", "centrum"]}
 
 # COMMAND ----------
 
@@ -90,7 +60,7 @@ DEVICE_TYPE_VALUES_TO_REPLACE = ["TV"]
 # COMMAND ----------
 
 dbutils.widgets.dropdown("target_name", "<no target>", ["<no target>"], "01. target name")
-dbutils.widgets.text("timestamp", "2023-08-03", "02. timestamp")
+dbutils.widgets.text("timestamp", "2020-12-12", "02. timestamp")
 dbutils.widgets.dropdown("sample_data", "complete", ["complete", "sample"], "03. sample data")
 
 # COMMAND ----------
@@ -98,11 +68,6 @@ dbutils.widgets.dropdown("sample_data", "complete", ["complete", "sample"], "03.
 widget_target_name = dbutils.widgets.get("target_name")
 widget_timestamp = dbutils.widgets.get("timestamp")
 widget_sample_data = dbutils.widgets.get("sample_data")
-
-# COMMAND ----------
-
-df_fs_metadata = spark.read.format("delta").load(get_value_from_yaml("paths", "feature_store_paths", "metadata"))
-display(df_fs_metadata)
 
 # COMMAND ----------
 
@@ -114,131 +79,105 @@ display(df_fs_metadata)
 
 # COMMAND ----------
 
-def get_features_to_load(categories: List[str], df: DataFrame) -> List[str]:
-
-    df_features = df.filter(F.col("category").isin(categories))
-
+def get_features_to_load(categories):
+    df_features = spark.read.table("odap_features_user.metadata").filter(
+        F.col("category").isin(categories)
+    )
     return [row.feature for row in df_features.collect()]
 
-list_categories = ["digital_interest", "digital_general", "digital_device"]
-df_features_to_load = get_features_to_load(list_categories, df_fs_metadata) 
-
-
-# COMMAND ----------
-
-def read_fs(feature_store, list_features):
-
-    return feature_store.select('user_id', 'timestamp', list_features).filter(F.col("timestamp") == F.lit(F.current_date()))
-#this reading will be modified 
-df = spark.read.format("delta").load(get_value_from_yaml("paths", "feature_store_paths", "user_entity_fs"))
-df_fs = read_fs(df, df_features_to_load)
+categories = ["digital_interest", "digital_general", "digital_device", "sociodemo_targets"]
+list_features_to_load = get_features_to_load(categories)
+print(list_features_to_load)
 
 # COMMAND ----------
 
-def join_urls(df_fs, df_url, logger):
-    df_joined = df_fs.join(
-        df_url.select("user_id", "timestamp", "collected_urls"),
-        on=["user_id", "timestamp"],
-        how="left",
-    )
-    logger.info(
-        f"Count of users with no URLs is {df_joined.select(F.sum(F.col('collected_urls').isNull().cast('integer'))).collect()[0][0]}."
-    )
+def read_fs(fs_date,features_to_load):
+    fs_stage2 = (
+        spark.read.table("odap_features_user.user_stage2")
+        .select("user_id", "timestamp", *features_to_load)
+        .filter(F.col("timestamp") == widget_timestamp))
+    return fs_stage2
+
+df_fs = read_fs(widget_timestamp, list_features_to_load)
+display(df_fs)
+
+# COMMAND ----------
+
+def join_urls(read_fs, df_url, logger: Logger):
+    df_joined = read_fs.join(df_url.select("user_id", "timestamp", "collected_urls"),on=["user_id", "timestamp"], how="left")
+    
+    logger.info(f"Count of users with no URLs is {df_joined.select(F.sum(F.col('collected_urls').isNull().cast('integer'))).collect()[0][0]}.")
+
     return df_joined
 
 df_income_url_scores = spark.read.format("delta").load(get_value_from_yaml("paths", "income_table_paths", "income_url_scores"))
-df_join_urls = join_urls(df_fs,  df_income_url_scores, root_logger)
+df_join_urls = join_urls(df_fs, df_income_url_scores, logger)
+display(df_join_urls)
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC
-# MAGIC
-# MAGIC ## Create URL flags
-# MAGIC We create binary integer flags for all URLs of interest.
+def get_cols_from_schema(schema):
+    unique_urls = []
+    columns_list = ["collected_urls"]
+    num_cols = []
+    cat_cols = []
+
+    for f in schema:
+
+        if f.name.split("_")[-1] == "flag":
+            unique_urls.append((".").join(f.name.split("_")[:-1]))
+            num_cols.append(f.name)
+
+        else:
+            columns_list.append(f.name)
+            if isinstance(f.dataType, StringType):
+                cat_cols.append(f.name)
+            elif isinstance(f.dataType, (IntegerType, DoubleType, FloatType, LongType)):
+                num_cols.append(f.name)
+            else:
+                raise Exception(f"{f.name} is unknown type {f.dataType}.") 
+    return unique_urls, columns_list, num_cols, cat_cols
 
 # COMMAND ----------
 
-def create_domain_features(df):
-    df = df.select(
-        "*",
-        *[
-            (F.array_contains("collected_urls", domain.replace("_", ".")).cast("int")).alias(f"{domain}_flag")
-            for domain in ALL_URLS_CLEANED
-        ]
-    )
+def get_schemas(models_dict):
+    with open(f'src/adpickercpex/solutions/sociodemo_training/schemas/socdemo_gender_schema_{models_dict["gender_male"].split("/")[-3]}.txt', 'r') as f:
+        schema_gender = eval(f.readlines()[0])
+
+
+    with open(f'src/adpickercpex/solutions/sociodemo_training/schemas/socdemo_age_schema_{models_dict["ageGroup_0_17"].split("/")[-3]}.txt', 'r') as f:
+        schema_age =  eval(f.readlines()[0])
+
+    schema_gender.extend(schema_age)
+    schema_both = list(set(schema_gender))
+    schema_both.remove(StructField('label', StringType(), True))
+
+    return schema_both
+
+models_dict = get_value_from_yaml("paths", "models", "sociodemo")
+schema = get_schemas(models_dict)
+print(schema)
+
+# COMMAND ----------
+
+unique_urls, columns_list, num_cols, cat_cols = get_cols_from_schema(schema)
+columns_list.extend(["user_id", "timestamp"])
+
+# COMMAND ----------
+
+def choose_features(df, columns_list):
+    df = (df.select(*columns_list,*[(F.array_contains(F.col("collected_urls"), domain).cast("int")).alias(f"{domain.replace('.', '_')}_flag") for domain in unique_urls])
+            .drop('collected_urls'))
     return df
 
-df_create_domin_features = create_domain_features(df_join_urls)
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC
-# MAGIC
-# MAGIC Specific columns are created for each gender and age group category to reduce the number of columns in the model (concentrate more info into less columns). 
-
-# COMMAND ----------
-
-def create_url_flags(df):
-    # define specific flagging function
-    def _add_category_url_features(df, _index):
-        df = df.select(
-            "*",
-            F.greatest(
-                *[F.col(f"{domain}_flag") for domain in ALL_URL_LISTS_CLEANED[2 * _index]]
-            ).cast("string").alias(f"{PREFIX_LIST[_index]}_primary_flag"),
-            F.greatest(
-                *[F.col(f"{domain}_flag") for domain in ALL_URL_LISTS_CLEANED[2 * _index + 1]]
-            ).cast("string").alias(f"{PREFIX_LIST[_index]}_secondary_flag"),
-        )
-        return df
-    
-    for _index in range(len(PREFIX_LIST)):
-        df = _add_category_url_features(df, _index)
-    
-    return df.drop("collected_urls", *[f"{domain}_flag" for domain in ALL_URLS_CLEANED])
-
-df_create_url_flags = create_url_flags(df_create_domin_features)
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC
-# MAGIC
-# MAGIC ## Prepare data for model fitting
-# MAGIC Replace null values and rarely occuring categorical variable levels.
-
-# COMMAND ----------
-
-
-def get_data_features(df):
-    skip_cols = [get_value_from_yaml("featurestorebundle", "entities", "user_entity", "id_column"), get_value_from_yaml("featurestorebundle", "entity_time_column")]
-    cat_cols = [
-        f.name
-        for f in df.schema.fields
-        if isinstance(f.dataType, T.StringType) and f.name not in skip_cols
-    ]
-    num_cols = [
-        f.name
-        for f in df.schema.fields
-        if isinstance(
-            f.dataType, (T.IntegerType, T.DoubleType, T.FloatType, T.LongType)
-        )
-        and f.name not in skip_cols
-    ]
-    
-    return {
-        "skip_cols" : skip_cols,
-        "num_cols" : num_cols,
-        "cat_cols" : cat_cols 
-    }
-
-df_get_data_features = get_data_features(df_create_url_flags)
+df_choose_features = choose_features(df_join_urls, columns_list)
+display(df_choose_features)
 
 # COMMAND ----------
 
 def replace_rare_values(df, num_cols, cat_cols):
+    for key, value in ALLOWED_VALUES.items():
+        df = df.withColumn(key, F.when(F.col(key).isin(value), F.col(key)).otherwise("None"))  
 
     df = replace_categorical_levels(
         df,
@@ -260,9 +199,11 @@ def replace_rare_values(df, num_cols, cat_cols):
 
     df = df.fillna(0, subset=num_cols)
     df = df.fillna("None", subset=cat_cols)
+    
     return df
 
-df_replace_rare_values = replace_rare_values(df_create_url_flags, df_get_data_features["num_cols"],  df_get_data_features["cat_cols"])
+df_replace_rare_values = replace_rare_values(df_choose_features, num_cols, cat_cols)
+display(df_replace_rare_values)
 
 # COMMAND ----------
 
@@ -294,7 +235,9 @@ def get_features(models_dict, table_name, category_name):
     }
     return features_dict
 
+models_dict = get_value_from_yaml("paths", "models", "sociodemo")
 metadata = get_features(get_value_from_yaml("paths", "models", "sociodemo"), "user", "sociodemo_features")
+
 
 # COMMAND ----------
 
@@ -305,9 +248,12 @@ metadata = get_features(get_value_from_yaml("paths", "models", "sociodemo"), "us
 
 # COMMAND ----------
 
-def apply_model(df: DataFrame, models_dict, logger):
+def apply_model(df, models_dict, logger: Logger):
+
     for model in models_dict:
+
         logger.info(f"Applying sociodemo model {model}, with path {models_dict[model]}")
+
         try:
             model_obj = mlflow.spark.load_model(models_dict[model])
             df = model_obj.transform(df)
@@ -346,16 +292,20 @@ def apply_model(df: DataFrame, models_dict, logger):
             df = df.withColumn(f"sociodemo_perc_{model}", F.lit(None).cast("double"))
             df = df.withColumn(f"sociodemo_prob_{model}", F.lit(None).cast("double"))
 
+
     # define values for male model as a complement to the female model
     df = df.withColumn(
-        "sociodemo_perc_gender_male", 1 - F.col("sociodemo_perc_gender_female")
+        "sociodemo_perc_gender_female", 1 - F.col("sociodemo_perc_gender_male")
     )
     df = df.withColumn(
-        "sociodemo_prob_gender_male", 1 - F.col("sociodemo_prob_gender_female")
+        "sociodemo_prob_gender_female", 1 - F.col("sociodemo_prob_gender_male")
     )
+
     return df
 
-df_apply_model = apply_model(df_replace_rare_values, get_value_from_yaml("paths", "models", "sociodemo"), root_logger)
+
+df_apply_model = apply_model(df_replace_rare_values, models_dict, logger)
+display(df_apply_model)
 
 # COMMAND ----------
 
@@ -371,17 +321,25 @@ def features_sociodemo(
 ):
 
     return df.select(
-        get_value_from_yaml("featurestorebundle", "entities", "user_entity", "id_column"),
-        get_value_from_yaml("featurestorebundle", "entity_time_column"),
+        "user_id",
+        "timestamp,
         *features_name,
     )
-df_final = features_socioemo(df_apply_model, list(metadata['features'].keys()))
+df_final = features_sociodemo(df_apply_model, list(metadata['features'].keys()))
 
 # COMMAND ----------
 
-# MAGIC %md
-# MAGIC ###Metadata
-
-# COMMAND ----------
-
-metadata = get_features(get_value_from_yaml("paths", "models", "sociodemo"), "user", "sociodemo_features")
+metadata =  {
+    "table":  "user_stage2",
+    "category": "sociodemo_features",
+    "features": {
+        "sociodemo_perc_{model_name}": {
+            "description": "sociodemo percentile for: {model_name}",
+            "fillna_with": -1.0,
+        },
+        "sociodemo_prob_{model_name}": {
+            "description": "sociodemo probability for: {model_name}",
+            "fillna_with": None,
+        },
+    }
+    }
