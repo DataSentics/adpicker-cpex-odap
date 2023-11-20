625d064a2f2357a8d7c0bd506f13d3c5718502e3
marek.tejc@datasentics.com <marek.tejc@datasentics.com>
2023-11-20 16:48:40 +0000

    Use proper secret

--- src/fs_connection.py
+++ src/fs_connection.py
@@ -3,4 +3,18 @@
 
 # COMMAND ----------
 
+dbutils.secrets.listScopes()
 
+# COMMAND ----------
+
+dbutils.secrets.list("adls-gen2-prod")
+
+# COMMAND ----------
+
+APP_ENV = "prod"
+
+# COMMAND ----------
+
+path_to_fs = f"abfss://gold@cpexstorageblob{APP_ENV}.dfs.core.windows.net/feature_store/features/user_entity.delta"
+df = spark.read.format("delta").load(path_to_fs)
+display(df)
